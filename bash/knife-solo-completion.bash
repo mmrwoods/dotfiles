7b5351cc97d4ab19efe317e8e4585975521175e7
Mark Woods <mwoods.online.ie@gmail.com>
2016-03-04 17:38:41 +0000

    Hack bash completion for knife solo data bag cmds
    Hack the utter crap out of knife solo completion to add completions for
    commands added by knife solo data bag gem. Everything is hard-coded, the
    check to determine whether the knife solo data bag gem is installed just
    greps Gemfile, and I managed to include three, nested, case statements!
    
    On the plus side, as everything is hard-coded, it is super fast :-)

--- bash/knife-solo-completion.bash
+++ bash/knife-solo-completion.bash
@@ -12,29 +12,58 @@ _knife_solo()
   local cur cmd
 
   cur=${COMP_WORDS[COMP_CWORD]}
-  cmd=${COMP_WORDS[2]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
 
-  case ${COMP_CWORD} in
-    1)
+  case ${prev} in
+    knife)
       COMPREPLY=($(compgen -W "solo" ${cur}))
       ;;
-    2)
-      COMPREPLY=($(compgen -W "bootstrap clean cook init prepare" ${cur}))
+    solo)
+      if [ -f Gemfile ] && grep knife-solo_data_bag Gemfile &> /dev/null; then
+        local solocmds="bootstrap clean cook data init prepare"
+      else
+        local solocmds="bootstrap clean cook init prepare"
+      fi
+      COMPREPLY=($(compgen -W "${solocmds}" ${cur}))
       ;;
-    3)
-      if [ "$cmd" != "init" ] && [ -d nodes ]; then
-        local nodes=`ls -1 nodes/*.json | sed 's/\(nodes\/\)\(.*\)\(.json\)/\2/'`
+    bootstrap|clean|cook|prepare)
+      if [ -d nodes ]; then
+        local nodes=`ls -1 nodes/*.json | xargs -n1 basename | sed 's/\(.*\)\(.json\)/\1/'`
         COMPREPLY=($(compgen -W "${nodes}" ${cur}))
       fi
       ;;
-    4)
-      if [ "$cmd" == "cook" ]; then
-        local configs=`ls -1 nodes/*.json`
-        COMPREPLY=($(compgen -W "${configs}" ${cur}))
-      fi
+    data)
+      COMPREPLY=($(compgen -W "bag" ${cur}))
       ;;
     *)
-      COMPREPLY=()
+      local solocmd=${COMP_WORDS[2]}
+      case ${solocmd} in
+        bootstrap|cook|prepare)
+          local configs=`ls -1 nodes/*.json`
+          COMPREPLY=($(compgen -W "${configs}" ${cur}))
+          ;;
+        data)
+          case ${prev} in
+            bag)
+              COMPREPLY=($(compgen -W "create edit list show" ${cur}))
+              ;;
+            edit|show)
+              local databags=`ls -1 data_bags/`
+              COMPREPLY=($(compgen -W "${databags}" ${cur}))
+              ;;
+            create|list)
+              COMPREPLY=()
+              ;;
+            *)
+              local items=`ls -1 data_bags/${prev}/*.json | xargs -n1 basename | sed 's/\(.*\)\(.json\)/\1/'`
+              COMPREPLY=($(compgen -W "${items}" ${cur}))
+              ;;
+          esac
+          ;;
+        *)
+          COMPREPLY=()
+          ;;
+      esac
       ;;
   esac
 }
