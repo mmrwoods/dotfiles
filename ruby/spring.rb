9dd58e33ca9faec56a546f1396f7e4d688e31b3c
Mark Woods <mwoods.online.ie@gmail.com>
2019-03-25 11:21:33 +0000

    Update spring config, warn when missing libs

--- ruby/spring.rb
+++ ruby/spring.rb
@@ -1,2 +1,7 @@
-require 'spring-commands-cucumber'
-require 'spring-commands-rspec'
+%w[spring-commands-cucumber spring-commands-rspec].each do |lib|
+  begin
+    require lib
+  rescue LoadError
+    warn "#{__FILE__}: #{$!.message}"
+  end
+end
